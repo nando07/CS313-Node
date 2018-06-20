953e652da223666852627905380ca517ebc2ea3f
Nando <redchameleonapps@gmail.com>
2018-06-20 15:04:46 -0500

    index.js

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  // Response ejs
-  .get('/getRate', (req, res) => {
+  .get('pages/getRate', (req, res) => {
     res.render('result.ejs')
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
